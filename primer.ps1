b22b02868b1254c0e16112cce22877159a6404f0
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2020-08-06 16:07:38 +0100

    _

--- primer.ps1
+++ primer.ps1
@@ -1,2 +1,7 @@
+choco install sqllocaldb -yr
+choco install sql-server-2017-cumulative-update -yr
+choco install sqlserver-cmdlineutils -yr
 
 Add-LocalDb PopulationHealth "$PSScriptRoot\.db"
+
+build PopulationHealth
