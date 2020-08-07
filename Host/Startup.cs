88d0dd373a24ea53ac85ab204bbdf297ca950e03
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2020-08-07 15:49:23 +0100

    _

--- Host/Startup.cs
+++ Host/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Sensemaking.Dapper;
 using Sensemaking.Query.Dapper;
@@ -14,8 +15,13 @@ namespace Host
 
         public override void ConfigureServices(IServiceCollection services)
         {
+            var config = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddJsonFile("localsettings.json", true, true)
+                .Build();
+
             base.ConfigureServices(services);
-            services.AddSingleton<IDb>(p => new Db(@"Server=(localdb)\PopulationHealth;Database=PopulationHealth;Integrated Security=True"));
+            services.AddSingleton<IDb>(p => new Db(config["ConnectionString"]));
         }
 
         public override void Configure(IApplicationBuilder app, IWebHostEnvironment env)
