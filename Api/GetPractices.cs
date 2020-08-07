abe4d8f706626a75c2a8a3e0e020e376b12198ae
David Williams <david.williams1@kantar.com>
2020-08-07 14:48:49 +0100

    _

--- Api/GetPractices.cs
+++ Api/GetPractices.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Threading.Tasks;
 using Sensemaking.Query.Dapper;
 using Sensemaking.Web.Api;
@@ -7,20 +8,30 @@ namespace Fdb.PopulationHealth.Api
 {
     public class GetPractices : IHandleGetRequests
     {
-        public string Route => "/";
+        private const string IcsRouteKey = "ics";
+
+        public string Route => $"/{{{IcsRouteKey}}}";
 
         public Task<object> Handle(RequestParameters parameters)
         {
-            return Task.FromResult<object>(new Query().GetResults(new Request()));
+            return Task.FromResult<object>(new Query().GetResults(new Request(parameters.Values[IcsRouteKey].ToString())));
         }
 
-        public class Request { }
+        public class Request
+        {
+            public Request(string ics)
+            {
+                Ics = ics;
+            }
+
+            public string Ics { get; }
+        }
         
         public class Query : Query<Request, HitCount>
         {
             public override HitCount[] GetResults(Request parameters)
             {
-                return database.Query<HitCount>("select * from RuleHitCount").ToArray();
+               return database.Query<HitCount>("select * from RuleHitCount where Ics = @ics", new { parameters.Ics }).ToArray();
             }
         }
     }
@@ -31,5 +42,6 @@ namespace Fdb.PopulationHealth.Api
         public string Name { get; } = null!;
         public string Description { get; } = null!;
         public int Count { get; }
+        public string Ics { get; } = null!;
     }
 }
--- Migrations/Scripts/0001-HitCountTable.sql
+++ Migrations/Scripts/0001-HitCountTable.sql
@@ -3,16 +3,17 @@ create table RuleHitCount (
 	[Practice] varchar(10) not null,
 	[Name] varchar(50) not null,
 	[Description] varchar(200) not null,
-	[Count] int
+	[Count] int,
+	[Ics] varchar(20) not null
 )
 
-insert into RuleHitCount select 'L83102001', 'Rule 1', 'A stupendously good rule.', 349
-insert into RuleHitCount select 'L83102001', 'Rule 2', 'An even better rule.', 21
-insert into RuleHitCount select 'L83102001', 'Rule 3', 'A rule for all occasions.', 1323
-insert into RuleHitCount select 'L83102001', 'Rule 4', 'Not another rule. I hate authority.', 812
-insert into RuleHitCount select 'L82003001', 'Rule 1', 'A stupendously good rule.', 649
-insert into RuleHitCount select 'L82003001', 'Rule 3', 'A rule for all occasions.', 146
-insert into RuleHitCount select 'L82003001', 'Rule 5', 'Rule makes me feel safe.', 6
-insert into RuleHitCount select 'L83100006', 'Rule 2', 'An even better rule.', 419
-insert into RuleHitCount select 'L83100006', 'Rule 4', 'Not another rule. I hate authority.', 1132
-insert into RuleHitCount select 'L82003004', 'Rule 3', 'A rule for all occasions.', 70
+insert into RuleHitCount select 'L83102001', 'Rule 1', 'A stupendously good rule.', 349, 'SYB'
+insert into RuleHitCount select 'L83102001', 'Rule 2', 'An even better rule.', 21, 'SYB' 
+insert into RuleHitCount select 'L83102001', 'Rule 3', 'A rule for all occasions.', 1323, 'SYB'
+insert into RuleHitCount select 'L83102001', 'Rule 4', 'Not another rule. I hate authority.', 812, 'SYB'
+insert into RuleHitCount select 'L82003001', 'Rule 1', 'A stupendously good rule.', 649, 'FRH'
+insert into RuleHitCount select 'L82003001', 'Rule 3', 'A rule for all occasions.', 146, 'FRH'
+insert into RuleHitCount select 'L82003001', 'Rule 5', 'Rule makes me feel safe.', 6, 'FRH'
+insert into RuleHitCount select 'L83100006', 'Rule 2', 'An even better rule.', 419, 'SYB'
+insert into RuleHitCount select 'L83100006', 'Rule 4', 'Not another rule. I hate authority.', 1132, 'SYB'
+insert into RuleHitCount select 'L82003004', 'Rule 3', 'A rule for all occasions.', 70, 'FRH'
