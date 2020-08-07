1c398178c311034289d9d9973dfca65838f8bd9b
David Williams <david.williams1@kantar.com>
2020-08-07 14:49:51 +0100

    _

--- Migrations/Scripts/0001-HitCountTable.sql
+++ Migrations/Scripts/0001-HitCountTable.sql
@@ -13,7 +13,7 @@ insert into RuleHitCount select 'L83102001', 'Rule 3', 'A rule for all occasions
 insert into RuleHitCount select 'L83102001', 'Rule 4', 'Not another rule. I hate authority.', 812, 'SYB'
 insert into RuleHitCount select 'L82003001', 'Rule 1', 'A stupendously good rule.', 649, 'FRH'
 insert into RuleHitCount select 'L82003001', 'Rule 3', 'A rule for all occasions.', 146, 'FRH'
-insert into RuleHitCount select 'L82003001', 'Rule 5', 'Rule makes me feel safe.', 6, 'FRH'
+insert into RuleHitCount select 'L82003001', 'Rule 5', 'Rules makes me feel safe.', 6, 'FRH'
 insert into RuleHitCount select 'L83100006', 'Rule 2', 'An even better rule.', 419, 'SYB'
 insert into RuleHitCount select 'L83100006', 'Rule 4', 'Not another rule. I hate authority.', 1132, 'SYB'
 insert into RuleHitCount select 'L82003004', 'Rule 3', 'A rule for all occasions.', 70, 'FRH'
